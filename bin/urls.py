654d5997a190c5528bdbb5eec41d7946d698132f
richard-deng <richard.deng@live.com>
2017-03-16 18:41:52 +0800

    修改

--- bin/urls.py
+++ bin/urls.py
@@ -12,52 +12,52 @@ from handler import eyesight
 urls = (
     ('/ping', ping.Ping),
     #页面
-    ('^/channel_op/v1/page/login.html$', login.Login),
-    ('^/channel_op/v1/page/index.html$', index.Index),
-    ('^/channel_op/v1/page/overview.html$', overview.OverView),
-    ('^/channel_op/v1/page/channel.html$', channel.ChannelManage),
-    ('^/channel_op/v1/page/store.html$', store.StoreManage),
-    ('^/channel_op/v1/page/device.html$', device.DeviceManage),
-    ('^/channel_op/v1/page/train/buyer.html$', train.TrainBuyerManage),
-    ('^/channel_op/v1/page/train/use.html$', train.TrainUseManage),
-    ('^/channel_op/v1/page/settle.html$', settle.SettleManage),
+    ('^/channel/v1/page/login.html$', login.Login),
+    ('^/channel/v1/page/index.html$', index.Index),
+    ('^/channel/v1/page/overview.html$', overview.OverView),
+    ('^/channel/v1/page/channel.html$', channel.ChannelManage),
+    ('^/channel/v1/page/store.html$', store.StoreManage),
+    ('^/channel/v1/page/device.html$', device.DeviceManage),
+    ('^/channel/v1/page/train/buyer.html$', train.TrainBuyerManage),
+    ('^/channel/v1/page/train/use.html$', train.TrainUseManage),
+    ('^/channel/v1/page/settle.html$', settle.SettleManage),
     #api
-    ('^/channel_op/v1/api/login$', login.LoginHandler),
-    ('^/channel_op/v1/api/sms_send$', login.SmsHandler),
-    ('^/channel_op/v1/api/passwd_change$', login.ChangePassHandler),
+    ('^/channel/v1/api/login$', login.LoginHandler),
+    ('^/channel/v1/api/sms_send$', login.SmsHandler),
+    ('^/channel/v1/api/passwd_change$', login.ChangePassHandler),
 
     #渠道API
-    ('^/channel_op/v1/api/channel_create$', channel.CreateChanHandler),
-    ('^/channel_op/v1/api/channel$', channel.ChanHandler),
-    ('^/channel_op/v1/api/channel_set_state', channel.ChanStateSetHandler),
-    ('^/channel_op/v1/api/chninfo_pagelist$', channel.ChannelInfoHandler),
-    ('^/channel_op/v1/api/chan_name_list$', channel.ChanNameList),
-    ('^/channel_op/v1/api/chan_store_list$', channel.ChanStoreMap),
+    ('^/channel/v1/api/channel_create$', channel.CreateChanHandler),
+    ('^/channel/v1/api/channel$', channel.ChanHandler),
+    ('^/channel/v1/api/channel_set_state', channel.ChanStateSetHandler),
+    ('^/channel/v1/api/chninfo_pagelist$', channel.ChannelInfoHandler),
+    ('^/channel/v1/api/chan_name_list$', channel.ChanNameList),
+    ('^/channel/v1/api/chan_store_list$', channel.ChanStoreMap),
 
 
     #门店API
-    ('^/channel_op/v1/api/store_set_state', store.StoreStateSetHandler),
-    ('^/channel_op/v1/api/store_create$', store.CreateStoreHandler),
-    ('^/channel_op/v1/api/store$', store.StoreHandler),
-    ('^/channel_op/v1/api/storeinfo_pagelist$', store.StoreInfoHandler),
-    ('^/channel_op/v1/api/store_eye$', store.StoreEyeHandler),
-    ('^/channel_op/v1/api/store_name_list$', store.StoreNameListHandler),
-    ('^/channel_op/v1/api/chan_store_total$', overview.OverViewInfoHandler),
+    ('^/channel/v1/api/store_set_state', store.StoreStateSetHandler),
+    ('^/channel/v1/api/store_create$', store.CreateStoreHandler),
+    ('^/channel/v1/api/store$', store.StoreHandler),
+    ('^/channel/v1/api/storeinfo_pagelist$', store.StoreInfoHandler),
+    ('^/channel/v1/api/store_eye$', store.StoreEyeHandler),
+    ('^/channel/v1/api/store_name_list$', store.StoreNameListHandler),
+    ('^/channel/v1/api/chan_store_total$', overview.OverViewInfoHandler),
 
 
-    ('^/channel_op/v1/api/devinfo_pagelist$', device.DeviceInfoHandler),
-    ('^/channel_op/v1/api/training_op_list$', train.TrainBuyInfoHandler),
-    ('^/channel_op/v1/api/training_use_list$', train.TrainUseInfoHandler),
+    ('^/channel/v1/api/devinfo_pagelist$', device.DeviceInfoHandler),
+    ('^/channel/v1/api/training_op_list$', train.TrainBuyInfoHandler),
+    ('^/channel/v1/api/training_use_list$', train.TrainUseInfoHandler),
 
     #视光师
-    ('^/channel_op/v1/api/eyesight_info$', eyesight.EyeSightInfoHandler),
+    ('^/channel/v1/api/eyesight_info$', eyesight.EyeSightInfoHandler),
 
     #训练订单
-    ('^/channel_op/v1/api/org_allot_to_chan_order$', train.OrgAllotToChanOrderHandler),
-    ('^/channel_op/v1/api/org_allot_to_store_order$', train.OrgAllotToStoreOrderHandler),
-    ('^/channel_op/v1/api/order_cancel$', train.OrderCancelHandler),
+    ('^/channel/v1/api/org_allot_to_chan_order$', train.OrgAllotToChanOrderHandler),
+    ('^/channel/v1/api/org_allot_to_store_order$', train.OrgAllotToStoreOrderHandler),
+    ('^/channel/v1/api/order_cancel$', train.OrderCancelHandler),
 
     #设备
-    ('^/channel_op/v1/api/create_device$', device.DeviceCreateHandler),
-    ('^/channel_op/v1/api/allocate_device$', device.DeviceAllocateHandler),
+    ('^/channel/v1/api/create_device$', device.DeviceCreateHandler),
+    ('^/channel/v1/api/allocate_device$', device.DeviceAllocateHandler),
 )
